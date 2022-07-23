f2e60a6d9467ed2144641efc193effa7ed5def0f
amtenu <amannov21@gmail.com>
2022-07-23 14:09:40 -0600

    Fix: Added input handler to the add new user component

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,101 +1,94 @@
-import React from 'react'
-import NavBar from '../../component/navbar/NavBar'
-import SideBar from '../../component/sidebar/SideBar'
-import "./new.scss"
+import React from "react";
+import NavBar from "../../component/navbar/NavBar";
+import SideBar from "../../component/sidebar/SideBar";
+import "./new.scss";
 
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
-import {
-
-
-  doc,
- 
-} from "firebase/firestore";
-import { serverTimestamp, setDoc } from 'firebase/firestore';
+import { addDoc, collection, doc } from "firebase/firestore";
+import { serverTimestamp, setDoc } from "firebase/firestore";
 import { useEffect } from "react";
 
-import { auth,storage } from '../../firebase';
-import { db } from '../../firebase';
+import { auth, storage } from "../../firebase";
+import { db } from "../../firebase";
 import { useNavigate } from "react-router-dom";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-
-
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
   const [data, setData] = useState({});
   const [per, setPerc] = useState(null);
 
-const navigate = useNavigate()
-useEffect(() => {
-  const uploadFile = () => {
-    const name = new Date().getTime() + file.name;
+  const navigate = useNavigate();
+  useEffect(() => {
+    const uploadFile = () => {
+      const name = new Date().getTime() + file.name;
 
-    console.log(name);
-    const storageRef = ref(storage, file.name);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+      console.log(name);
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        setPerc(progress);
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-          default:
-            break;
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          setPerc(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prev) => ({ ...prev, img: downloadURL }));
+          });
         }
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setData((prev) => ({ ...prev, img: downloadURL }));
-        });
-      }
-    );
+      );
+    };
+    file && uploadFile();
+  }, [file]);
+
+
+  console.log(data);
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
   };
-  file && uploadFile();
-}, [file]);
 
-
-
-
-
-
-
-
-  const handleAdd=async(e)=>{
+  const handleAdd = async (e) => {
     e.preventDefault();
-    try{
-      const res=await createUserWithEmailAndPassword(
+    try {
+    const res= await createUserWithEmailAndPassword(
         auth,
         data.email,
         data.password
       );
-      console.log(res);
-      await setDoc(
-        doc(db, "users", res.user.uid),
-        { ...data,timeStamp:serverTimestamp() });
-          navigate(-1);
-    }catch(error){
+      
+
+      await addDoc(collection(db, "users"), {
+        ...data,       
+        createdAt: serverTimestamp(),
+      });
+      navigate(-1);
+    } catch (error) {
       console.log(error);
     }
-  }
-
-
-
-
+  };
 
 
 
@@ -119,7 +112,7 @@ useEffect(() => {
             />
           </div>
           <div className="right">
-            <form onSubmit={handleAdd}    >
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
@@ -135,7 +128,12 @@ useEffect(() => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                  id={input.id}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    onChange={handleInput}
+                  />
                 </div>
               ))}
               <button type="submit">Send</button>

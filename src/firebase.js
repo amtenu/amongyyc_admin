e6127bf977c6b271c446aad97e7e2ecf90d48205
amtenu <amannov21@gmail.com>
2022-07-23 04:51:43 -0600

    initial setup Firebase and auth

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeA3tfGHGIYKaXtNQ64UH0K-ZGqKeUsHs",
+  authDomain: "amongyyc-ebf99.firebaseapp.com",
+  projectId: "amongyyc-ebf99",
+  storageBucket: "amongyyc-ebf99.appspot.com",
+  messagingSenderId: "983376128809",
+  appId: "1:983376128809:web:81900b03b478fd9577e538",
+};
+
+// Initialize Firebase and export it as a singleton
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
+export const storage = getStorage(app);

81d7418e05a224747357c1635675fab7c40904d8
amtenu <amannov21@gmail.com>
2022-07-23 08:52:29 -0600

    storing current user to local storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
+//change the current user to object and choose null or user
 const INITIAL_STATE = {
   currentUser: JSON.parse(localStorage.getItem("user")) || null,
 };
@@ -10,6 +11,8 @@ export const AuthContext = createContext(INITIAL_STATE);
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
+
+  //store in local storage
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.currentUser));
   }, [state.currentUser]);

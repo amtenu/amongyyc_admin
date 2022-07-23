b13fc38b4cfafc2816e81f9e0b22784a87c64ea9
amtenu <amannov21@gmail.com>
2022-07-23 02:31:17 -0600

    user and Task form input management

---
+++ src/formSource.js
@@ -0,0 +1,92 @@
+const  userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "john_doe",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "John Doe",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "john_doe@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+1 234 567 89",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Elton St. 216 NewYork",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "USA",
+    },
+  ];
+  
+ const taskInputs = [
+    {
+      id: 1,
+      label: "type",
+      title: "text",
+      placeholder: "imgupload",
+    },
+    {
+      id: 2,
+      label: "Title",
+      type: "text",
+      placeholder: "Bow Building upload",
+    },
+    {
+      id: 3,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 4,
+      label: "Address",
+      type: "text",
+      placeholder: "Address",
+    },
+    {
+      id: 5,
+      label: "Latitude",
+      type: "text",
+      placeholder: "Latitude",
+    },
+    {    
+      id: 6,
+      label: "Longitude",
+      type: "text",
+      placeholder: "Longitude",
+    },
+    {
+      id: 7,
+      label: "Region",
+      type: "text",
+      placeholder: "Region",
+    },
+    
+  ];
+
+
+    export  {userInputs,taskInputs};
